8effe1e42434f1edf322b3e1bbb556eb0b6e4554
Tybern82 <jeffpublic@kintoshmalae.com>
2021-10-04 10:35:14 +1000

    Initial work on BrimeAPI v1 (with OAuth)
    API appears to have been changed with introduction of OAuth, however is still listing with the v1 indication, so we have retained the same versioning information, but chosen to specify the OAuth version separate from the previous v1 API.

---
+++ BrimeAPI-v1-OAuth/tv/brime/api/EUserRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BrimeAPIv1OAuth.tv.brime.api {
+    [Flags] public enum EUserRoles : ushort {
+        NONE = 0,
+        STAFF = 1,
+        PLATFORM_MOD = 2,
+        VERIFIED = 4,
+        BRIME_PRO = 8,
+        BOT = 16,
+        USER = 32,
+        CREATOR = 64,
+        SUBSCRIBER = 128,
+        FOLLOWER = 256,
+        MODERATOR = 512
+    }
+}
